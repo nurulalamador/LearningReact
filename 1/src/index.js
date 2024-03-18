359bc349ba10cd4faaaab88b3062f17dc6e0b4c9
nurulalamador <66767267+nurulalamador@users.noreply.github.com>
2024-03-18 13:02:55 +0600

    Add files via upload

---
+++ 1/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Tanvir } from './tanvir';
+
+const root = ReactDOM.createRoot(document.getElementById("main"));
+
+root.render(
+  <>
+  Hello {Tanvir} <br/>
+  My name is {2*2}<br/>
+  </>
+);
+
---
+++ 1/src/tanvir.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export function Tanvir() {
+    return (<>Hello Worlds`@G</>);
+}
+
+export function Ador() {
+    return (<>Hello Worlds`@G</>);
+}
+
+//export default Tanvir();
